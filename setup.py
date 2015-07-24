ce141f2a74f6b857b71d4bb007ce724a880130a6
Evgeniy Tatarkin <tatarkin.evg@gmail.com>
2015-07-24 16:33:23 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='pipybage',
+    name='pybadge',
     version='0.1',
-    description='OpenShift App',
+    description='OpenShift App for generating badges',
     author='Tatarkin Evgeniy S.',
     author_email='tatarkin.evg@gmail.com',
     url='http://www.python.org/sigs/distutils-sig/',
